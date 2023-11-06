c140661e1df3880c7c2d56293986211520fdd101
BADuprey <111667673+BADuprey@users.noreply.github.com>
2023-11-06 15:06:04 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,10 +6,11 @@
         <meta name="author" content="Evan Trafton and Brodie Duprey">
         <meta name="description" content="Class Exercise">
 </head>
+
+<body>
 <nav>
     <a href="protected/index.php">Protected</a>
 </nav>
-<body>
     <h1>Landing page, not password protected</h1>
 </body>
 </html>
