d1cb41fbdf4459c7521082e393765e16493bfee9
Evan Trafton <Evan.Trafton@uvm.edu>
2023-11-06 14:37:07 -0500

    Added Programmer 3 index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang='en'>
+    <head>
+    <meta charset="utf-8">
+    <meta name="author" content="Evan Trafton and Brodie Duprey">
+        <meta name="description" content="Class Exercise">
+    </head>
+    <body>
+        <h1>password protected</h1>
+</body>
+</html>
