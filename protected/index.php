015a86557f55c36562712af6d027afcbde504b3b
Evan Trafton <Evan.Trafton@uvm.edu>
2023-11-06 14:57:05 -0500

    added code in the index.php in the protected folder

--- protected/index.php
+++ protected/index.php
@@ -1,10 +1,20 @@
 <!DOCTYPE html>
 <html lang='en'>
     <head>
-    <meta charset="utf-8">
-    <meta name="author" content="Evan Trafton and Brodie Duprey">
+        <meta charset="utf-8">
+        <meta name="author" content="Evan Trafton and Brodie Duprey">
         <meta name="description" content="Class Exercise">
+        <?php
+            $allowedNetIds = array('rerickso','ettrafto','baduprey');
+
+            $netId = htmlentities($_SERVER["REMOTE_USER"], ENT_QUOTES, "UTF-8");
+        
+        
+        ?>
+    
     </head>
+
+
     <body>
         <h1>password protected</h1>
 </body>
