d55b926839d5a0e683a2631421542ba52f579257
BADuprey <111667673+BADuprey@users.noreply.github.com>
2023-11-06 14:41:59 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang='en'>
+    <head>
+        <meta charset="utf-8">
+        <title>HT ACCESS example</title>
+        <meta name="author" content="Evan Trafton and Brodie Duprey">
+        <meta name="description" content="Class Exercise">
+</head>
+<nav>
+    <a href="../protected">Protected</a>
+</nav>
+<body>
+    <h1>Landing page, not password protected</h1>
+</body>
+</html>
